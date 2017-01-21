2f303628144bd3bd5e5d5685db6f613b71a364f0
Langston Barrett <langston.barrett@gmail.com>
2017-01-21 12:19:27 +0000

    shell.nix: cleanup

--- shell.nix
+++ shell.nix
@@ -1,14 +1,13 @@
-# To use: nix-shell --run zsh
+# To use: run `nix-shell` or `nix-shell --run "exec zsh"`
 # https://nixos.org/wiki/Development_Environments
 # http://nixos.org/nix/manual/#sec-nix-shell
 
 let
   _nixpkgs = import <nixpkgs> { };
-  # 16.09 from https://github.com/NixOS/nixpkgs/releases
   pkgs = import (_nixpkgs.fetchFromGitHub {
     owner = "NixOS";
     repo  = "nixpkgs";
-    #rev   = "16.09"; # build fails mysteriously...
+    # 16.09 fails to build, so this is a recent (2017-01) master
     rev   = "81d9893bcd9d96b4c7aabea4381f6765289ef722";
     sha256 = "0phjhlxlb4cwyrpj9gpz3caf13qbawijr7rq46a8id5ak92kawah";
   }) {};
